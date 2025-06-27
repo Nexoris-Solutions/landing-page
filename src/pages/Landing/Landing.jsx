559a7f5d52a734967d58e12736d68459e85b132c
r-rajaneesh <rajaneeshr2006@gmail.com>
2025-06-27 21:39:11 +0530

    ➕ Finalised Contact page design.

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Hero from "../../components/Hero/Hero";
-
+import Contacts from "../../components/Contacts/Contacts";
 function Landing() {
 	return (
 		<div className="min-h-screen h-full w-screen flex flex-col">
 			<Header />
 			<Hero />
+			<Contacts />
 			<Footer />
 		</div>
 	);
